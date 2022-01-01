cf68c89494254d00dd8f34276376d99553965ffc
Searchstars <2039210460@qq.com>
2022-01-01 18:45:53 +0800

    完工

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Windows;
 using ICSharpCode.SharpZipLib.Zip;
 using IWshRuntimeLibrary;
+using ModernWpf.Controls;
 
 namespace System_Init_Toolbox___Setup
 {
@@ -158,6 +159,51 @@ namespace System_Init_Toolbox___Setup
             else
             {
                 MessageBox.Show("Error", "安装失败",MessageBoxButton.OK,MessageBoxImage.Error);
+                Environment.Exit(0);
+            }
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string path = string.Empty;
+            System.Windows.Forms.FolderBrowserDialog scpath = new System.Windows.Forms.FolderBrowserDialog();
+            if (scpath.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                path = scpath.SelectedPath;
+                //由于是自选目录所以几乎不可能存在没这路径的情况，自然没必要进行判断了。
+                //复制文件
+                status_label.Content = "复制文件包中...";
+                pb.Value = 25;
+                Delay(1000);//给予label和pb.value切换的反应时间
+                string sourcePath = "win10-x64.zip";//自解压后程序压缩包名
+                string targetPath = path;//目标位置 直接默认目录
+                bool isrewrite = true; // true=覆盖已存在的同名文件,false则反之
+                System.IO.File.Copy(sourcePath, targetPath, isrewrite);//复制~
+                                                                       //解压文件
+                status_label.Content = "解压文件包中...";
+                pb.Value = 75;
+                Delay(1000);
+                if (UnZip(path + "/win10-x64.zip", path, null))//如果解压成功
+                {
+                    System.IO.File.Delete(path + "/win10-x64.zip");//删除残留文件包
+                    CreateShortcut("%USERPROFILE%/Desktop", "System Init Toolbox", path + "/System Init Toolbox.exe", "【System Init Toolbox · 系统初始化工具箱】一个能让你在安装系统后快速安装运行库等必备程序的软件", path + "/System Init Toolbox.exe");//创建桌面快捷方式
+                    CreateShortcut(@"C:\ProgramData\Microsoft\Windows\Start Menu\Programs", "System Init Toolbox", path + "/System Init Toolbox.exe", "【System Init Toolbox · 系统初始化工具箱】一个能让你在安装系统后快速安装运行库等必备程序的软件", path + "/System Init Toolbox.exe");//创建任务栏快捷方式
+                }
+                else
+                {
+                    MessageBox.Show("Error", "安装失败", MessageBoxButton.OK, MessageBoxImage.Error);
+                    Environment.Exit(0);
+                }
+            }
+            else
+            {
+                ContentDialog noselectDialog = new ContentDialog
+                {
+                    Title = "未选择目录",
+                    Content = "请选选择一个用于存放程序文件的目录再继续安装哦~若您不想选择，直接以默认路径安装也是可以的哦！",
+                    CloseButtonText = "Ok"
+                };
+                ContentDialogResult result = await noselectDialog.ShowAsync();
             }
         }
     }
---
+++ SetupBATFiles/README.txt
@@ -0,0 +1,2 @@
+SetupBATFiles文件夹应该放在程序（System Init Toolbox - Setup.exe）根目录下（仅为自解压程序打包前的目录而非自解压前的目录）。
+放在此处只是为了方便查看，不代表实际使用意义。
